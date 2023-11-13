e96de840a77dc082fd51ba9dc526b76103a45188
Krishnasankar <sankarkrishna689@gmail.com>
2023-11-13 18:30:50 +0530

    Update settings.py

--- instagram/settings.py
+++ instagram/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lv%9ja!e7+z#alt1sdd#12*zp-z=!4a+43apx=-01df25ef8hg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['keralagram.vercel.app', '127.0.0.1','.vercel.app', '.now.sh ']
 
