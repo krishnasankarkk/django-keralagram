176d196fac02c5b81e965685cd42854977120d12
krishnasankar <sankarkrishna689@gmail.com>
2023-11-23 20:45:34 +0530

    bug fix

--- users/models.py
+++ users/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
-# from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class UserAccount(models.Model):
@@ -10,7 +9,6 @@ class UserAccount(models.Model):
     bio = models.TextField(blank=True,null=True)
     phone = models.CharField(max_length=10,blank=True,null=True)
     profile_pic = models.ImageField(upload_to="uploads/",blank=True,null=True)
-    # profile_pic = CloudinaryField('image')
     followers = ArrayField(models.IntegerField(blank=True),blank=True,null=True,default=list)
     following = ArrayField(models.IntegerField(blank=True),blank=True,null=True,default=list)
     posts = models.IntegerField(default=0)
