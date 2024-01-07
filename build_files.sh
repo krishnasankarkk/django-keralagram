6536e144653da5a24844a0db4cef1ee654fe526f
krishnasankar <sankarkrishna689@gmail.com>
2024-01-07 19:28:09 +0530

    build

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo "BUILD START"
 python3 -m venv .venv
-./venv/scripts/activate 
-python3 -m pip install -r requirements.txt
-python3 -m manage collectstatic --noinput --clear
+source .venv/bin/activate  
+./.venv/bin/python -m pip install -r requirements.txt
+./.venv/bin/python -m manage collectstatic --noinput --clear
 echo "BUILD END"
