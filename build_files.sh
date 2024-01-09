75e89c265420b51c9ab5001a7f1538a129ac9c3d
krishnasankar <sankarkrishna689@gmail.com>
2024-01-09 15:37:01 +0530

    cloudinary config

--- build_files.sh
+++ build_files.sh
@@ -3,4 +3,5 @@ python3 -m venv .venv
 source .venv/bin/activate  
 ./.venv/bin/python3 -m pip install --upgrade pip
 ./.venv/bin/python3 -m pip install -r requirements.txt
+./.venv/bin/python3 -m manage collectstatic --noinput --clear
 echo "BUILD END"
